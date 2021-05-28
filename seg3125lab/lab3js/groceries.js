2827781b4aa35dc01c447a63408aae41a742ecdc
Chetan <ChetanMandur45@gmail.com>
2021-05-28 18:34:16 -0400

    inital lab3

---
+++ seg3125lab/lab3js/groceries.js
@@ -0,0 +1,113 @@
+	
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products		 
+
+var products = [
+    //No restrictions
+	{
+		name: "Brocoli",
+		lactoseFree: true,
+		nutFree: true,
+        organic: true,
+		price: 1.99
+	},
+	{
+		name: "Bread",
+		lactoseFree: true,
+		nutFree: true,
+        organic: false,
+		price: 2.35
+	},
+	{
+		name: "Salmon",
+		lactoseFree: true,
+		nutFree: true,
+        organic: false,
+		price: 10.00
+	},
+    {
+        name: "Lettuce",
+        lactoseFree: true,
+        nutFree: true,
+        organic: true,
+        price: 7.50,
+    },
+    //Lactose products
+    {
+        name: "Cheese",
+        lactoseFree:false,
+        nutFree: true, 
+        organic: true,
+        price: 5.00,
+    },
+    {
+        name:"Milk",
+        lactoseFree:false,
+        nutFree: true,
+        organic: false,
+        price:6.00,
+    },
+    {
+        name:"Yogurt",
+        lactoseFree:false,
+        nutFree: true,
+        organic: false,
+        price: 3.20,
+    },
+    //Nut products
+    {
+        name:"Almonds",
+        lactoseFree: true,
+        nutFree: false,
+        organic: true,
+        price: 1.25,
+    },
+    {
+        name: "Peanut Butter",
+        lactoseFree: true,
+        nutFree: false,
+        organic: true,
+        price: 4.50,
+    },
+    {
+        name: "Cookies",
+        lactoseFree: true,
+        nutFree: false,
+        organic: false,
+        price: 6.24,
+    }
+
+];
+	
+
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(prods, nut, lactose, organic) {
+    let products_filtered = [...prods]
+
+    if (nut == true){
+        products_filtered = products_filtered.filter(function(a){return a.nutFree==true});
+    }
+    if (lactose==true){
+        products_filtered = products_filtered.filter(function(a){return a.lactoseFree==true});
+    }
+    if (organic==true){
+        products_filtered = products_filtered.filter(function(a){return a.organic==true});
+    }
+    products_filtered.sort(function(a, b){return a.price - b.price});
+
+	return products_filtered;
+}
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+		}
+	}
+	return totalPrice.toFixed(2);
+}
